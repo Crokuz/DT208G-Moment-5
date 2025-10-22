2c64631ccfc1db182162a7d707fe030935afdea4
Crokuz <leander.norberg@hotmail.com>
2025-10-22 21:04:43 +0200

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Courses } from './pages/courses/courses';
+import { Schedule } from './pages/schedule/schedule';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "courses", component: Courses},
+    {path: "schedule", component: Schedule},
+    {path: "", redirectTo: "schedule", pathMatch: "full"}
+];
